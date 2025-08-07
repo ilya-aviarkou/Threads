5b71a6b297ef2cf56da92496b7a162f35acaf116
ilya-aviarkou <ilya.averkov1@gmail.com>
2025-08-07 22:57:32 +0300

    implement base tab-views ui

---
+++ Threads/Core/Explore/View/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  Threads
+//
+//  Created by Ilya Averkov on 7.08.25.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("Explore view")
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ Threads/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Threads
+//
+//  Created by Ilya Averkov on 7.08.25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
